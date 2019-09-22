61a3d4fb9b2c41998dfa8db4af8d31f0d124646f
s3722763 <s3722763@student.rmit.edu.au>
2019-09-22 21:17:48 +1000

    Started work on I2C

--- Assignment/main.c
+++ Assignment/main.c
@@ -6,8 +6,12 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
-#define LCD_ADDRESS 0x20
+#define LCD_ADDRESS 0x27
+#define WRITE 0xFE
+//For 50 kHz
+#define I2C_RATE_REGISTER 0x70
 
 typedef struct Ultrasonic_t {
 	uint16_t distance;
@@ -15,44 +19,74 @@ typedef struct Ultrasonic_t {
 	struct Ultrasonic* newData;
 } Ultrasonic;
 
-void querySensors(Ultrasonic* leftSensor, Ultrasonic* rightSensor)
-{
-	register int *lower_time asm("r24");
-	register int *upper_time asm("r25");
-	
-	PORTB = 0x42;
-	while(PINB == 0x42);
-	
-	//TODO: Maybe use hardware interrupts when learned
-	__asm("loop:");
-	__asm("IN r20, 0x16");
-	__asm("CPI r20, 0x42");
-	__asm("BRNE FIND_HIGH");
-	
-	__asm("FIND_HIGH:");
-	__asm("CPI r20, 0xC3");
-	__asm("BRNE CHECK_LEFT");
-	__asm("ADIW r24, 0x4");
-	__asm("RJMP loop");
-	
-	__asm("CHECK_LEFT:");
-	__asm("ANDI r20, 0x40");
-	__asm("BRNE CHECK_RIGHT")
-	
-	__asm("END: NOP");
+void setBaudRate(void) {
+	//Setup baudrate
+	TWSR = 0x00;
+	TWBR = I2C_RATE_REGISTER;
+	TWCR = (1 << TWEN);
+}
+
+void error(uint8_t code) {
+	PORTB = code;
+	while(1);
+}
+
+void twiStart(void) {
+	//Send START condition
+	uint8_t current_status = TWSR;
+	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+	//Wait for start condition to be fully transmitted
+	while(!(TWCR & ((1 << TWINT) | (1 << TWEN))));
+	if ((TWSR & 0xF8) != current_status) {
+		error(0x1);
+	}
+}
+
+void twiSendAddress(void) {
+	uint8_t current_status = TWSR;
+	uint8_t toSend = (LCD_ADDRESS << 1) & WRITE; // 7 bit address and write bit as LSB as 0
+	TWDR = toSend;
+	TWCR = (1 << TWINT) | (1 << TWEN); //Send address
+	while(!(TWCR & ((1 << TWINT) | (1 << TWEN )))); // Wait until sent
+	if ((TWSR & 0xF8) != current_status) {
+		error(0x2);
+	}
+}
+
+void twiSentData(uint8_t data) {
+	uint8_t current_status = TWSR;
+	TWDR = data; //Set data to be sent
+	TWCR = (1 << TWINT) | (1 << TWEN); //Send data
+	while(!(TWCR & ((1 << TWINT) | (1 << TWEN )))); // Wait until sent
+	if ((TWSR & 0xF8) != current_status) {
+		error(0x1);
+	}
+}
+
+void twiStop(void) {
+	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWSTO);
+}
+
+void sendTWI(uint8_t msg) {
+	//PORTB = 0x10;
+	twiStart();
+	//PORTB = 0x20;
+	twiSendAddress();
+	//PORTB = 0x30;
+	twiSentData(msg);
+	//PORTB = 0x40;
+	twiStop();
 }
 
 int main(void)
 {
     Ultrasonic leftSensor;
     Ultrasonic rightSensor;
-    /*10111101*/
-    DDRB = 0xBD;
-	//Enable pull-up resistors
-	/*01000010*/
-	PINB = 0x42;
+	DDRB = 0xFF;
+	sei();
+	setBaudRate();
+	sendTWI(0xF0);
 	
-	querySensors(&leftSensor, &rightSensor);
 	
     while (1) 
     {
