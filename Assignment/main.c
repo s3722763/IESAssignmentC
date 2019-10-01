68f435dde4b9e5e06028febfc772895e3249b0cb
s3722763 <s3722763@student.rmit.edu.au>
2019-10-01 23:31:21 +1000

    Ultrasonic sensor now works

--- Assignment/main.c
+++ Assignment/main.c
@@ -5,99 +5,72 @@
  * Author : Daniel
  */ 
 
+#define F_CPU 12000000UL
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
-uint8_t volatile loop = 0;
-uint8_t volatile up = 0;
-
-void serialSetup() {
-	UBRRH = 0x00;
-	UBRRL = 0x4D;
-	
-	// Enable transmit and recieve bits
-	UCSRB = (1 << RXEN) | (1 << TXEN);
-	// Async operation no parity 8 bits
-	UCSRC = (1 << URSEL) | (3 << UCSZ0);
-}
-
-void serialSend(uint8_t msg) {
-	while ((UCSRA & (1 << UDRE)) == 0);
-	UDR = msg;
-}
-
-ISR(TIMER1_OVF_vect){}
+uint8_t volatile got_distance = 0;
+uint8_t volatile was_high = 0;
 
 ISR(INT1_vect) {
-	if (up == 1) {
-		PORTB = 0xF0;
-		//Set timer1 counter to zero
-		TCNT1 = 0x0000;
-		//Start clock
-		TCCR1B |= (1 << CS10);
-		//Enable overflow interrupt for timer1
-		TIMSK = (1 << TOIE1);
+	if (was_high == 1) {
+		//Stop timer
+		TCCR1B = 0;
+		got_distance = 1;
+		was_high = 0;
 	} else {
-		//PORTB = 0x0F;
-		//On falling edge
-		//TCCR1B &= ~(1 << CS10);
-		//loop = 0;
+		//Start timer
+		TCCR1B |= (1 << CS10);
+		was_high = 1;
 	}
 }
 
-typedef struct Ultrasonic_t {
-	uint16_t distance;
-	uint16_t time;
-	struct Ultrasonic* newData;
-} Ultrasonic;
-
-uint16_t querySensors(Ultrasonic* leftSensor, Ultrasonic* rightSensor)
+uint16_t querySensors()
 {	
 	uint8_t i;
-	uint16_t time = 0;
 	PORTC = 0x01;
 	//About 10 us
-	while (i++ < 14);
+	_delay_us(10);
 	PORTC = 0x00;
 	
-	loop = 1;
-	up = 1;
-	//Enable interrupt
-	GICR = 0x80;
-	while(loop != 0 );
+	got_distance = 0;
+	while(got_distance == 0);
 	
-	time = TCNT1 / 12;
+	uint16_t clock_cycles = TCNT1;
+	TCNT1 = 0;
 	//About 12 clock cycles a us
-	//serialSend((uint8_t)(time >> 8));
-	//serialSend((uint8_t)(time &0x00FF));
-	return time;
+	return clock_cycles;
 }
 
 uint8_t timeToDistance(uint16_t time) {
 	uint8_t distance;
-	distance = time / 58;
+	distance = time / 696;
 	return distance;
 }
 
 int main(void)
 {
-    Ultrasonic leftSensor;
-    Ultrasonic rightSensor;
-    /*10000000*/
 	DDRB = 0xFF;
 	DDRD = 0b11110111;
-	PORTB = 0x00;
     DDRC = 0x01;
 	
-	//Set interupt to any change
-	MCUCR = (1 << ISC10) | (1 << ISC11);
-	//serialSend((MCUCR));
-	serialSetup();
+	PORTB = 0x00;
+	PORTC = 0x00;
+	
+	//Enable interrupt
+	GICR |= (1 << INT1);
+	//Changes on any logical change
+	MCUCR |= (1 << ISC10);
 	sei();
 	
-	uint16_t time_s = querySensors(&leftSensor, &rightSensor);
-	uint8_t distance = timeToDistance(time_s);
-	PORTB = distance;
+	while (1) {
+		uint16_t time_s = querySensors();
+		uint8_t distance = timeToDistance(time_s);
+		PORTB = distance;
+		_delay_ms(50);
+	}
 	
 	//serialSend(distance);
     while (1);
